3d830f7b6c8e70f99af998471a578f5d7534c861
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2023-04-18 08:21:40 -0400

    Update FloatyPatches.cs

--- Patches/FloatyPatches.cs
+++ Patches/FloatyPatches.cs
@@ -12,7 +12,8 @@ namespace TisButAScratch.Patches
 
         static class AbstractActor_CheckPilotStatusFromAttack
         {
-
+            [HarmonyPriority(Priority.Last)]
+            public static bool Prepare() => false;
             static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilGenerator)
             {
                 List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
@@ -23,26 +24,32 @@ namespace TisButAScratch.Patches
                 MethodInfo clearNeedsInjuryMI = AccessTools.DeclaredMethod(typeof(Pilot), "ClearNeedsInjury");
 
                 int injuryStrIdx = 0, clearInjuryIdx = 0;
+                var foundIdx = false;
                 for (int i = 0; i < codes.Count; i++)
                 {
                     CodeInstruction instruction = codes[i];
                     if (instruction.opcode == OpCodes.Ldstr && "{0}: PILOT INJURED".Equals((string)instruction.operand, StringComparison.InvariantCultureIgnoreCase))
                     {
                         injuryStrIdx = i;
+                        foundIdx = true;
                         ModInit.modLog?.Info?.Write($"Found PILOT INJURED instruction at idx: {i}");
                     }
                     else if (instruction.opcode == OpCodes.Callvirt && (MethodInfo)instruction.operand == clearNeedsInjuryMI)
                     {
                         clearInjuryIdx = i;
+                        foundIdx = true;
                         ModInit.modLog?.Info?.Write($"Found Pilot.ClearNeedsInjury instruction at idx: {i}");
                     }
                 }
 
-                CodeInstruction cnjInstruction = codes[clearInjuryIdx - 1];
-                cnjInstruction.labels.Add(clearNeedsInjuryLabel);
+                if (foundIdx )
+                {
+                    CodeInstruction cnjInstruction = codes[clearInjuryIdx - 1];
+                    cnjInstruction.labels.Add(clearNeedsInjuryLabel);
 
-                codes.RemoveRange(injuryStrIdx, 15);
-                codes.Insert(injuryStrIdx - 1, new CodeInstruction(OpCodes.Br_S, clearNeedsInjuryLabel));
+                    codes.RemoveRange(injuryStrIdx, 15);
+                    codes.Insert(injuryStrIdx - 1, new CodeInstruction(OpCodes.Br_S, clearNeedsInjuryLabel));
+                }
 
                 return codes;
             }
@@ -51,22 +58,29 @@ namespace TisButAScratch.Patches
         [HarmonyPatch(typeof(Mech), "CompleteKnockdown")]
         static class Mech_CompleteKnockdown
         {
+            [HarmonyPriority(Priority.Last)]
+            public static bool Prepare() => false;
             static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
                 List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
 
                 int targetIdx = 0;
+                var foundIdx = false;
                 for (int i = 0; i < codes.Count; i++)
                 {
                     CodeInstruction instruction = codes[i];
                     if (instruction.opcode == OpCodes.Ldstr && "KNOCKDOWN: PILOT INJURED".Equals((string)instruction.operand, StringComparison.InvariantCultureIgnoreCase))
                     {
                         targetIdx = i;
+                        foundIdx = true;
                         ModInit.modLog?.Info?.Write($"KNOCKDOWN: PILOT INJURED instruction at idx: {i}");
                     }
                 }
-                codes.RemoveRange(targetIdx - 4, 12);
 
+                if (foundIdx)
+                {
+                    codes.RemoveRange(targetIdx - 4, 12);
+                }
                 return codes;
             }
         }
